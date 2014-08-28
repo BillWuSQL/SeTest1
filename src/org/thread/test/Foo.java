53346dcd49211ddb990ac086e32f8ce47bf016e1
BillWuSQL <274322055@qq.com>
2014-08-28 10:35:25 +0800

    third commit

---
+++ src/org/thread/test/Foo.java
@@ -0,0 +1,15 @@
+package org.thread.test;
+
+
+public class Foo {
+    private int x = 100;
+
+    public int getX() {
+        return x;
+    }
+
+    public int fix(int y) {
+        x = x - y;
+        return x;
+    }
+}
--- src/org/thread/test/TestMitiThread1.java
+++ src/org/thread/test/TestMitiThread1.java
@@ -6,11 +6,22 @@ public class TestMitiThread1 implements Runnable {
 
 	public static void main(String[] args) {
 		System.out.println(Thread.currentThread().getName() + " 线程运行开始!");
+		
 		TestMitiThread1 test = new TestMitiThread1();
 		Thread th1 = new Thread(test);
 		Thread th2 = new Thread(test);
+//		th1.setPriority(Thread.MAX_PRIORITY);
 		th1.start();
+//		th1.setDaemon(true);
+		th1.setPriority(9);
+		System.out.println("th1是否后台线程: ============= " + th1.isDaemon());
+		System.out.println("th1线程优先级: ============= " + th1.getPriority());
+//		th2.setDaemon(true);
 		th2.start();
+		System.out.println("th2是否后台线程: ============= " + th2.isDaemon());
+		System.out.println("th2线程优先级: ============= " + th2.getPriority());
+
+
 		System.out.println(Thread.currentThread().getName() + " 线程运行结束!");
 		ArrayList sa;
 	}
@@ -21,7 +32,9 @@ public class TestMitiThread1 implements Runnable {
 		// TODO Auto-generated method stub
 		System.out.println(Thread.currentThread().getName() + " 线程运行开始!");
 		for (int i = 0; i < 10; i++) {
-			System.out.println();
+//			System.out.println();
+//			System.out.println();
+//			System.out.println();
             System.out.println(i + " " + Thread.currentThread().getName());
             try {
                 Thread.sleep((int) Math.random() * 10);
